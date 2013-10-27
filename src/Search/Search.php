a7109d4a103cfc5f73bd8e19f9773c61c30adae2
webuser <ryan@virtuecenter.com>
2013-10-26 22:19:47 -0400

    getting started with elastic search

--- src/Search/Search.php
+++ src/Search/Search.php
@@ -1,4 +1,28 @@
 <?php
 namespace Search;
 
-class Search {}
+class Search {
+	private $searchGateway;
+	private $root;
+
+	public function __construct ($searchGateway, $root) {
+		$this->searchGateway = $searchGateway;
+		$this->root = $root;
+	}
+
+	public function index ($type='all', $id, $body, $index=false) {
+		if ($index === false) {
+			$index = $this->root;
+		}
+		$params = [];
+		$params['body'] = $body;
+		$params['index'] = $index;
+		$params['type'] = $type;
+		$params['id'] = (string)$id;
+		return $this->searchGateway->index($params);
+	}
+
+	public function search () {
+
+	}
+}
