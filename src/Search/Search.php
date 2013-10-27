1a15fb322d636d4bd0a1b7e3df3a9aaa5a42476d
webuser <ryan@virtuecenter.com>
2013-10-27 08:37:10 -0400

    basic functionality working

--- src/Search/Search.php
+++ src/Search/Search.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * virtuecenter\search
+ *
+ * Copyright (c)2013 Ryan Mahoney, https://github.com/virtuecenter <ryan@virtuecenter.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 namespace Search;
 
 class Search {
@@ -7,10 +30,10 @@ class Search {
 
 	public function __construct ($searchGateway, $root) {
 		$this->searchGateway = $searchGateway;
-		$this->root = $root;
+		$this->root = trim(str_replace('/', '__', $root), '__');
 	}
 
-	public function index ($type='all', $id, $body, $index=false) {
+	public function index ($id, $body, $type=null, $index=false) {
 		if ($index === false) {
 			$index = $this->root;
 		}
@@ -22,7 +45,28 @@ class Search {
 		return $this->searchGateway->index($params);
 	}
 
-	public function search () {
+	public function search ($query, $type=null, $index=false) {
+		if ($index === false) {
+			$index = $this->root;
+		}
+		$searchParams['index'] = $index;
+		$searchParams['type'] = $type;
+		if (is_string($query)) {
+			$searchParams['body']['query']['query_string']['query'] = $query;
+		} else {
+			$searchParams['body']['query'] = $query;
+		}
+		return $this->searchGateway->search($searchParams);
+	}
 
+	public function delete ($id, $type, $index=false) {
+		if ($index === false) {
+			$index = $this->root;
+		}
+		$deleteParams = [];
+		$deleteParams['index'] = $index;
+		$deleteParams['type'] = $type;
+		$deleteParams['id'] = $id;
+		return $this->searchGateway->delete($deleteParams);
 	}
 }
